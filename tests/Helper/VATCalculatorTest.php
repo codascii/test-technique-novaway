43a528420bb29b0b447e1630725a825a492639bf
Mohamed Houmadi <dev.codascii@gmail.com>
2024-06-08 20:29:42 +0200

    Correction des tests déjà écrits

--- tests/Helper/VATCalculatorTest.php
+++ tests/Helper/VATCalculatorTest.php
@@ -15,9 +15,7 @@ class VATCalculatorTest extends TestCase
         // Net = 96 €
         $this->assertEquals(
             VATCalculator::getPriceArray(96),
-            ['netPrice' => 96, 'vat' => 16, 'rawPrice' => 80,],
-            '',
-            0.01
+            ['netPrice' => 96, 'vat' => 19.2, 'rawPrice' => 76.8]
         );
 
         // Raw : 3141.59 €
@@ -25,9 +23,7 @@ class VATCalculatorTest extends TestCase
         // Net = 3769.91 €
         $this->assertEquals(
             VATCalculator::getPriceArray(3769.91),
-            ['netPrice' => 3769.91, 'vat' => 628.32, 'rawPrice' => 3141.59,],
-            '',
-            0.01
+            ['netPrice' => 3769.91, 'vat' => 753.98, 'rawPrice' => 3015.93]
         );
 
         // Raw : 16 €
@@ -35,9 +31,7 @@ class VATCalculatorTest extends TestCase
         // Net = 19.20 €
         $this->assertEquals(
             VATCalculator::getPriceArray(19.20),
-            ['netPrice' => 19.20, 'vat' => 3.2, 'rawPrice' => 16,],
-            '',
-            0.01
+            ['netPrice' => 19.20, 'vat' => 3.84, 'rawPrice' => 15.36]
         );
 
         // Raw : 10287.5 €
@@ -45,9 +39,7 @@ class VATCalculatorTest extends TestCase
         // Net = 12345 €
         $this->assertEquals(
             VATCalculator::getPriceArray(12345),
-            ['netPrice' => 12345, 'vat' => 2057.50, 'rawPrice' => 10287.5,],
-            '',
-            0.01
+            ['netPrice' => 12345, 'vat' => 2469, 'rawPrice' => 9876]
         );
     }
 }
